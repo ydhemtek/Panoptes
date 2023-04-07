b1ae01c34b1f9b2b2332ac2cac33939b30ad1b6d
ydhemtek <mehdy.abadi@epitech.eu>
2023-04-07 16:26:18 +0200

    [edit] module enterprise, [add] choice 2 and 3

--- modules/enterprise.py
+++ modules/enterprise.py
@@ -1,6 +1,5 @@
 #priority:0
 from Panoptes import *
-from bs4 import BeautifulSoup
 from undetected_chromedriver import Chrome, ChromeOptions
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -52,6 +51,42 @@ def enterp():
             element = driver.find_element(By.XPATH, '//*[@id="search_details"]')
             print(element.text)
     if (number == 2):
-        print("available soon")
+        company = str(input("Enter a name of a company : "))
+        with Chrome(options=options) as driver:
+            driver.get(f"https://www.societe.com/cgi-bin/search?champs={company}")
+            time.sleep(3)
+            button = driver.find_element(By.XPATH, '//*[@id="didomi-popup"]/div/div/div/span')
+            button.click()
+            time.sleep(3)
+            button2 = driver.find_element(By.XPATH, '//*[@id="result_deno_societe"]/p/a')
+            button2.click()
+            time.sleep(2)
+            for i in tqdm(range(4), desc="data processing...", unit="s", unit_scale=True):
+                time.sleep(1)
+            element = driver.find_element(By.XPATH, '//*[@id="container"]/div')
+            print(element.text)
     if (number == 3):
-        print("available soon")
+        siren = str(input("Enter a siren number : "))
+        with Chrome(options=options) as driver:
+            driver.get(f"https://www.societe.com/cgi-bin/search?champs={siren}")
+            time.sleep(3)
+            button = driver.find_element(By.XPATH, '//*[@id="didomi-popup"]/div/div/div/span')
+            button.click()
+            time.sleep(3)
+            button2 = driver.find_element(By.XPATH, '//*[@id="result_deno_societe"]')
+            button2.click()
+            element2 = driver.find_element(By.XPATH, '//*[@id="presentation"]')
+            for i in tqdm(range(4), desc="data processing...", unit="s", unit_scale=True):
+                time.sleep(1)
+            print(element2.text)
+            element = driver.find_element(By.XPATH, '//*[@id="renseignement"]')
+            for i in tqdm(range(4), desc="data processing...", unit="s", unit_scale=True):
+                time.sleep(1)
+            print(element.text)
+            button4 = driver.find_element(By.XPATH, '//*[@id="cartodir"]/a[2]')
+            button4.click()
+            time.sleep(3)
+            element3 = driver.find_element(By.XPATH, '//*[@id="minifichecarto"]/div')
+            for i in tqdm(range(4), desc="data processing...", unit="s", unit_scale=True):
+                time.sleep(1)
+            print(element3.text)
